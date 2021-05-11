598a4d24ed4a07bd0e34424e7b264c5d20246af6
Subodh Lonkar <62687864+learner-subodh@users.noreply.github.com>
2021-05-11 20:49:25 +0530

    Update detect_emotions.py

--- detect_emotions.py
+++ detect_emotions.py
@@ -5,6 +5,7 @@ import numpy as np
 import os
 import tensorflow as tf
 from tensorflow import keras
+import keras
 from keras.models import Model, model_from_json
 import time
 from bokeh.models.widgets import Div
