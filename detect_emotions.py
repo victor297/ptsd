7a294e5414645a2d58d65a2669b759801cd9983b
Subodh Lonkar <62687864+learner-subodh@users.noreply.github.com>
2021-05-13 17:15:17 +0530

    Update detect_emotions.py

--- detect_emotions.py
+++ detect_emotions.py
@@ -63,36 +63,70 @@ def main():
     """Face Expression Detection App"""
     #setting the app title & sidebar
 
-    activities = ["Home", "Detect your Facial expressions" , "See Model Performance", "About"]
+    activities = ["Home", "Recognize My Facial Expressions" , "See Model Performance", "About"]
     choice = st.sidebar.selectbox("Select Activity", activities)
 
     if choice == 'Home':
         html_temp = """ 
         <marquee behavior="scroll" direction="left" width="100%;">
-        <h2 style= "color: #445eff; font-family: 'Raleway',sans-serif; font-size: 44px; font-weight: 700; line-height: 102px; margin: 0 0 24px; text-align: center; text-transform: uppercase;">How are you feeling today? </h2>
+        <h2 style= "color: #000000; font-family: 'Raleway',sans-serif; font-size: 44px; font-weight: 700; line-height: 102px; margin: 0 0 24px; text-align: center; text-transform: uppercase;">How are you feeling today? </h2>
         </marquee><br>
         """
         st.markdown(html_temp, unsafe_allow_html=True)
 
-        st.header("Video Demo: ")
+        st.subheader("**Video Demo: **")
         st.subheader(":smile: :hushed: :worried: :rage: :fearful:")
-        st.header("\nWill be uploaded soon!")
+        st.markdown("____")
+        st.markdown("**Working on bringing it to you...**")
+        st.markdown("**Will be uploaded soon!**")
 
     if choice == 'See Model Performance':
-        st.title("Face Expression WEB Application :")
-        st.subheader(":smile: :hushed: :worried: :rage: :fearful:")
-        st.subheader("\nWorking on bringing it to you...")
-        st.subheader("Will be back with it soon!")
+        st.title("Web Application For Facial Expression Recognition!")
+        #html_choice = """ 
+        #<marquee behavior="scroll" direction="left" width="100%;">
+        #<h2 style= "color: #000000; font-family: 'Raleway',sans-serif; font-size: 44px; font-weight: 700; line-height: 102px; margin: 0 0 24px; text-align: center; text-transform: uppercase;">Facial Expression Recognition Web Application </h2>
+        #</marquee><br>
+        #"""
+        #st.markdown(html_choice, unsafe_allow_html=True)
 
-    if choice == 'Detect your Facial expressions':
-        st.title("WEB Application For Detection of Facial Expressions:")
+        st.subheader(":smile: :hushed: :worried: :rage: :fearful:")
+        st.markdown("____")
+        #st.subheader("\nWorking on bringing it to you...")
+        #st.subheader("Will be back with it soon!")
+        actions = ["Train and Test Accuracies", "Evaluation of the Model" , "Confusion Matrix"]
+        action = st.sidebar.selectbox("Choose One", actions)
+        #st.markdown("____")
+        #if st.checkbox('Train and Test Accuracies'):
+        if action == 'Train and Test Accuracies':
+            st.subheader('**Train Accuracy vs. Test Accuracy**')
+            st.image('model_accuracy.JPG', width=700)
+        #st.markdown("____")
+        #if st.checkbox('\n\nEvaluation of the Model'):
+        if action == 'Evaluation of the Model':
+            st.subheader('**Evaluation of the Model**')
+            st.image('model_metrics.JPG', width=700, height=700)
+        #st.markdown("____")
+        #if st.checkbox('\n\nConfusion Matrix'):
+        if action == 'Confusion Matrix':
+            st.subheader('**Confusion Matrix using all the classes**')
+            st.image('model_cm.JPG', width=700)
+
+    if choice == 'Recognize My Facial Expressions':
+        st.title("Web Application For Facial Expression Recognition!")
+        #html_choice = """ 
+        #<marquee behavior="scroll" direction="left" width="100%;">
+        #<h2 style= "color: #000000; font-family: 'Raleway',sans-serif; font-size: 44px; font-weight: 700; line-height: 102px; margin: 0 0 24px; text-align: center; text-transform: uppercase;">Facial Expression Recognition Web Application </h2>
+        #</marquee><br>
+        #"""
+        #st.markdown(html_choice, unsafe_allow_html=True)
+
         st.subheader(":smile: :hushed: :worried: :rage: :fearful:")
         image_file = st.file_uploader("Upload Image", type=['jpg','png','jpeg'])
 
         #if image if uploaded, display the progress bar and the image
         if image_file is not None:
             our_image = Image.open(image_file)
-            st.text("Original Image")
+            st.markdown("**Original Image**")
             progress = st.progress(0)
             for i in range(100):
                 time.sleep(0.01)
@@ -100,13 +134,14 @@ def main():
             st.image(our_image)
         if image_file is None:
             st.error("No image uploaded yet")
+            return
 
         # Face Detection
         task = ["Faces"]
         feature_choice = st.sidebar.selectbox("Find Features", task)
         if st.button("Process"):
             if feature_choice == 'Faces':
-                st.text("Processing...\n")
+                st.markdown("**Processing...\n**")
 
                 #Progress bar
                 progress = st.progress(0)
@@ -120,26 +155,37 @@ def main():
                     st.success("Found {} faces".format(len(result_faces)))
 
                     if prediction == 'Happy':
-                        st.subheader("YeeY!  You are Happy :smile: today , Always Be! ")
+                        st.subheader("YeeY! You look **_Happy_** :smile: today, always be! ")
                     elif prediction == 'Angry':
-                        st.subheader("You seem to be angry :rage: today ,Take it easy! ")
+                        st.subheader("You seem to be **_Angry_** :rage: today, just take it easy! ")
                     elif prediction == 'Disgust':
-                        st.subheader("You seem to be Disgust :rage: today! ")
+                        st.subheader("You seem to be **_Disgusted_** :rage: today! ")
                     elif prediction == 'Fear':
-                        st.subheader("You seem to be Fearful :fearful: today ,Be couragous! ")
+                        st.subheader("You seem to be **_Fearful_** :fearful: today, be couragous! ")
                     elif prediction == 'Neutral':
-                        st.subheader("You seem to be Neutral today ,Happy day! ")
+                        st.subheader("You seem to be **_Neutral_** today, wish you a happy day! ")
                     elif prediction == 'Sad':
-                        st.subheader("You seem to be Sad :sad: today ,Smile and be happy! ")
+                        st.subheader("You seem to be **_Sad_** :worried: today, smile and be happy! ")
                     elif prediction == 'Surprise':
-                        st.subheader("You seem to be Surprised today! ")
+                        st.subheader("You seem to be **_Surprised_** today! ")
                     else :
-                        st.error("Your image does not match the training dataset's images! Try an other image!")
+                        st.error("Your image does not seem to match the training dataset's images! Please try another image!")
                     
     elif choice == 'About':
-        st.title("Face Expression WEB Application :")
-        st.subheader(":smile: :worried: :fearful: :rage: :hushed:")
-        st.subheader("About Face Expression Detection App")
+        st.title("Web Application For Facial Expression Recognition!")
+        #html_choice = """ 
+        #<marquee behavior="scroll" direction="left" width="100%;">
+        #<h2 style= "color: #000000; font-family: 'Raleway',sans-serif; font-size: 44px; font-weight: 700; line-height: 102px; margin: 0 0 24px; text-align: center; text-transform: uppercase;">Facial Expression Recognition Web Application </h2>
+        #</marquee><br>
+        #"""
+        #st.markdown(html_choice, unsafe_allow_html=True)
 
+        st.subheader(":smile: :worried: :fearful: :rage: :hushed:")
+        st.markdown("____")
+        st.markdown("**Dataset used for training:** https://www.kaggle.com/deadskull7/fer2013")
+        st.subheader("About me:")
+        st.markdown("**LinkedIn:** https://www.linkedin.com/in/subodh-lonkar-47662819b/")
+        st.markdown("**GitHub:** https://github.com/learner-subodh")
+        st.markdown("**Medium:** https://medium.com/@learner.subodh")
 
 main()
